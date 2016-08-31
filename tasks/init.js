62608913cd166b991d59d5b6f7ba4c34b8aed071
rand0me <dandydan2k@gmail.com>
2016-08-31 18:59:36 +0300

    Added message and instructions

--- tasks/init.js
+++ tasks/init.js
@@ -7,6 +7,7 @@ const greplace = require('gulp-replace');
 const grename = require('gulp-regex-rename');
 const gdebug = require('gulp-debug');
 const path = require('path');
+const message = require('../lib/message');
 
 const templatesBase = path.join(__dirname, '../templates/');
 
@@ -26,17 +27,21 @@ module.exports = function init() {
       log.writeln(log.red(log.bold('init') + ' requires a ' + log.bold('name') + ' parameter'));
       log.writeln();
       log.writeln(log.gray('Example: ' + log.bold('ihpm init kebab-cased-name')));
+      return false;
     }
 
     const camelName = name.replace( /(^|-)(\w)/g, (m, $1, $2) => $2.toUpperCase() );
-
-    log.writeln("Using template: " + log.bold(templates.javascript));
 
     gulp.src(templates.javascript, { dot: true })
       .pipe(greplace(/test-module/g, name))
       .pipe(greplace(/TestModule/g, camelName))
       .pipe(grename(/TestModule/, camelName))
       .pipe(gulp.dest(path.join(process.cwd(), 'inhabit-module-' + name)))
+      .on('end', () => {
+        message();
+        log.writeln(log.green("\nDone!"));
+        log.writeln(log.blue(`Tip: try to "cd inhabit-module-${name}" and run "npm install", then "npm run watch"`))
+      })
       .pipe(gdebug({ title: 'ihpm init:' }));
 };
 
